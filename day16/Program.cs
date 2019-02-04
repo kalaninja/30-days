4c53b542d6e38b119106a6d60adcacac97d52dd8
Alexander Kalankhodzhaev <kalansoft@gmail.com>
2019-02-04 15:57:09 +0300

    day 16

---
+++ day16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        Console.WriteLine(
+            int.TryParse(input, out var number)
+                ? number.ToString()
+                : "Bad String");
+    }
+}
