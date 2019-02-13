7278a6fc168ba35a6c3eaf3c5dc0a71fcd94a0af
kalan <kalansoft@gmail.com>
2019-02-13 16:09:05 +0300

    day 8 rewrite

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,39 +1,35 @@
 use std::collections::HashMap;
-use std::io::{self, BufRead};
+use std::io::BufRead;
 
 fn main() {
-    let n = read_n();
-    let mut phone_book = HashMap::new();
+    let n = read_ln().trim().parse().unwrap();
+    let phone_book = (0..n).map(|_| read_entry())
+        .fold(HashMap::new(), |mut acc, x| {
+            acc.insert(x.0, x.1);
+            acc
+        });
 
-    for _ in 0..n {
-        let entry = read_string();
-        let mut iterator = entry.trim().split_whitespace();
-        let name = iterator.next().unwrap().to_owned();
-        let phone = iterator.next().unwrap().to_owned();
-
-        phone_book.insert(name, phone);
-    }
-
-    let stdin = io::stdin();
-    let lines = stdin.lock().lines();
-    for query in lines {
-        let q = query.unwrap();
-        let name = q.trim();
-        match phone_book.get::<str>(&name) {
-            Some(x) => println!("{}={}", name, x),
-            None => println!("Not found")
-        }
-    }
+    std::io::stdin().lock().lines()
+        .map(|x| x.unwrap().trim().to_string())
+        .for_each(|name| {
+            match phone_book.get::<str>(&name) {
+                Some(phone) => println!("{}={}", name, phone),
+                None => println!("Not found")
+            }
+        });
 }
 
-fn read_n() -> i32 {
+fn read_ln() -> String {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().parse().unwrap()
-}
-
-fn read_string() -> String {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    std::io::stdin().read_line(&mut input).unwrap();
     input
+}
+
+fn read_entry() -> (String, String) {
+    let ln = read_ln();
+    let mut iter = ln.trim().split_whitespace();
+    let name = iter.next().unwrap().to_string();
+    let phone = iter.next().unwrap().to_string();
+
+    (name, phone)
 }
